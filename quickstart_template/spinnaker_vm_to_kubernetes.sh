94b67948307cd5ba6fc82b253577df80a4644800
Eric Jizba <erijiz@microsoft.com>
2017-03-09 09:11:17 -0800

    Clean up quickstart template scripts
    Allow jenkins scripts to pass in artifacts location
    Improve variable handling/docs for spinnaker script

--- quickstart_template/spinnaker_vm_to_kubernetes.sh
+++ quickstart_template/spinnaker_vm_to_kubernetes.sh
@@ -1,26 +1,142 @@
 #!/bin/bash
 
-while getopts :i:p:s:t:u:g:f:c:n:k:r:e:a:o:l:y:x: option; do
-  case "${option}" in
-        i) client_id="${OPTARG}";;
-        p) client_key="${OPTARG}";;
-        s) subscription_id="${OPTARG}";;
-        t) tenant_id="${OPTARG}";;
-        u) admin_user_name="${OPTARG}";;
-        g) resource_group="${OPTARG}";;
-        f) master_fqdn="${OPTARG}";;
-        c) master_count="${OPTARG}";;
-        n) storage_account_name="${OPTARG}";;
-        k) storage_account_key="${OPTARG}";;
-        r) azure_container_registry="${OPTARG}";;
-        e) include_kubernetes_pipeline="${OPTARG}";;
-        l) pipeline_registry="${OPTARG}";;
-        y) pipeline_repository="${OPTARG}";;
-        x) pipeline_port="${OPTARG}";;
-        a) artifacts_location="${OPTARG}";;
-        o) artifacts_location_sas_token="${OPTARG}";;
-    esac
+function print_usage() {
+  cat <<EOF
+https://github.com/Azure/azure-quickstart-templates/tree/master/spinnaker-vm-to-kubernetes
+Command
+  $0
+Arguments
+  --client_id|-ci                    [Required] : Service principal client id  used to dynamically manage resource in your subscription
+  --client_key|-ck                   [Required] : Service principal client key used to dynamically manage resource in your subscription
+  --subscription_id|-si              [Required] : Subscription Id
+  --tenant_id|-ti                    [Required] : Tenant Id
+  --user_name|-un                    [Required] : Admin user name for your Spinnaker VM and Kubernetes cluster
+  --resource_group|-rg               [Required] : Resource group containing your Kubernetes cluster
+  --master_fqdn|-mf                  [Required] : Master FQDN of your Kubernetes cluster
+  --master_count|-mc                 [Required] : Master count of your Kubernetes cluster
+  --storage_account_name|-san        [Required] : Storage Account name used for Spinnaker's persistent storage
+  --storage_account_key|-sak         [Required] : Storage Account key used for Spinnaker's persistent storage
+  --azure_container_registry|-acr    [Required] : Azure Container Registry url
+  --include_kubernetes_pipeline|-ikp            : Include a kubernetes pipeline (off by default).
+  --pipeline_registry|-prg                      : Registry to target in the pipeline
+  --pipeline_repository|-prp                    : Repository to target in the pipeline
+  --pipeline_port|-pp                           : Port to target in your pipeline
+  --artifacts_location|-al                      : Url used to reference other scripts/artifacts.
+  --sas_token|-st                               : A sas token needed if the artifacts location is private.
+EOF
+}
+
+function throw_if_empty() {
+  local name="$1"
+  local value="$2"
+  if [ -z "$value" ]; then
+    echo "Parameter '$name' cannot be empty." 1>&2
+    print_usage
+    exit -1
+  fi
+}
+
+#Set defaults
+include_kubernetes_pipeline="0"
+pipeline_registry="index.docker.io"
+pipeline_repository="lwander/spin-kub-demo"
+pipeline_port="8000"
+artifacts_location="https://raw.githubusercontent.com/Azure/azure-devops-utils/master/"
+
+while [[ $# > 0 ]]
+do
+  key="$1"
+  shift
+  case $key in
+    --client_id|-ci)
+      client_id="$1"
+      shift
+      ;;
+    --client_key|-ck)
+      client_key="$1"
+      shift
+      ;;
+    --subscription_id|-si)
+      subscription_id="$1"
+      shift
+      ;;
+    --tenant_id|-ti)
+      tenant_id="$1"
+      shift
+      ;;
+    --user_name|-un)
+      user_name="$1"
+      shift
+      ;;
+    --resource_group|-rg)
+      resource_group="$1"
+      shift
+      ;;
+    --master_fqdn|-mf)
+      master_fqdn="$1"
+      shift
+      ;;
+    --master_count|-mc)
+      master_count="$1"
+      shift
+      ;;
+    --storage_account_name|-san)
+      storage_account_name="$1"
+      shift
+      ;;
+    --storage_account_key|-sak)
+      storage_account_key="$1"
+      shift
+      ;;
+    --azure_container_registry|-acr)
+      azure_container_registry="$1"
+      shift
+      ;;
+    --include_kubernetes_pipeline|-ikp)
+      include_kubernetes_pipeline="$1"
+      shift
+      ;;
+    --pipeline_registry|-prg)
+      pipeline_registry="$1"
+      shift
+      ;;
+    --pipeline_repository|-prp)
+      pipeline_repository="$1"
+      shift
+      ;;
+    --pipeline_port|-pp)
+      pipeline_port="$1"
+      shift
+      ;;
+    --artifacts_location|-al)
+      artifacts_location="$1"
+      shift
+      ;;
+    --sas_token|-st)
+      artifacts_location_sas_token="$1"
+      shift
+      ;;
+    --help|-help|-h)
+      print_usage
+      exit 13
+      ;;
+    *)
+      echo "ERROR: Unknown argument '$key'" 1>&2
+      exit -1
+  esac
 done
+
+throw_if_empty --client_id $client_id
+throw_if_empty --client_key $client_key
+throw_if_empty --subscription_id $subscription_id
+throw_if_empty --tenant_id $tenant_id
+throw_if_empty --user_name $user_name
+throw_if_empty --resource_group $resource_group
+throw_if_empty --master_fqdn $master_fqdn
+throw_if_empty --master_count $master_count
+throw_if_empty --storage_account_name $storage_account_name
+throw_if_empty --storage_account_key $storage_account_key
+throw_if_empty --azure_container_registry $azure_container_registry
 
 spinnaker_kube_config_file="/home/spinnaker/.kube/config"
 kubectl_file="/usr/local/bin/kubectl"
@@ -40,7 +156,7 @@ azure login --service-principal -u $client_id -p $client_key --tenant $tenant_id
 azure account set $subscription_id
 
 # Copy kube config to this VM
-curl --silent "${artifacts_location}spinnaker/copy_kube_config/copy_kube_config.sh${artifacts_location_sas_token}" | sudo bash -s -- -un "$admin_user_name" -rg "$resource_group" -mf "$master_fqdn" -mc "$master_count"
+curl --silent "${artifacts_location}spinnaker/copy_kube_config/copy_kube_config.sh${artifacts_location_sas_token}" | sudo bash -s -- -un "$user_name" -rg "$resource_group" -mf "$master_fqdn" -mc "$master_count"
 
 # If targeting docker, we have to explicitly add the repository to the config. For private registries, 
 # there's no need because Spinnaker can dynamically retrieve the entire catalog of a registry.
@@ -56,8 +172,8 @@ curl --silent "${artifacts_location}spinnaker/configure_k8s/configure_k8s.sh${ar
 # Install and setup Kubernetes cli for admin user
 sudo curl -L -s -o $kubectl_file https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 sudo chmod +x $kubectl_file
-mkdir -p /home/${admin_user_name}/.kube
-sudo cp $spinnaker_kube_config_file /home/${admin_user_name}/.kube/config
+mkdir -p /home/${user_name}/.kube
+sudo cp $spinnaker_kube_config_file /home/${user_name}/.kube/config
 
 # Create pipeline if enabled
 if (( $include_kubernetes_pipeline )); then
@@ -69,7 +185,7 @@ if (( $include_kubernetes_pipeline )); then
 
         # Install docker CLI
         curl -sSL https://get.docker.com/ | sh
-        sudo gpasswd -a $admin_user_name docker
+        sudo gpasswd -a $user_name docker
 
         # Add (virtually) empty container to ACR to properly initialize Spinnaker. This fixes two bugs:
         # 1. The pipeline isn't triggered on the first push to the ACR (according to the source code, Igor "avoids publishing an event if this account has no indexed images (protects against a flushed redis)")
