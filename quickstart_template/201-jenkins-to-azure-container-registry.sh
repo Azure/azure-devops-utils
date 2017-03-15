798f0be33d30080ea5d15f6633cbc0a7aa968502
Claudiu Guiman <clguiman@microsoft.com>
2017-03-15 14:18:44 -0700

    Pass the artifact location

--- quickstart_template/201-jenkins-to-azure-container-registry.sh
+++ quickstart_template/201-jenkins-to-azure-container-registry.sh
@@ -114,5 +114,5 @@ if [[ "${include_docker_build_pipeline}" == "1" ]]
 then
     echo "Including the pipeline"
 
-    curl --silent "${artifacts_location}/jenkins/add-docker-build-job.sh${artifacts_location_sas_token}" | sudo bash -s -- -j "http://localhost:8080/" -ju "admin" -g "${git_url}" -r "${registry}" -ru "${registry_user_name}"  -rp "${registry_password}" -rr "$repository" -sps "* * * * *"
+    curl --silent "${artifacts_location}/jenkins/add-docker-build-job.sh${artifacts_location_sas_token}" | sudo bash -s -- -j "http://localhost:8080/" -ju "admin" -g "${git_url}" -r "${registry}" -ru "${registry_user_name}"  -rp "${registry_password}" -rr "$repository" -sps "* * * * *" -al "$artifacts_location" -st "$artifacts_location_sas_token"
 fi
