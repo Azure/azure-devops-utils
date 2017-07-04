8c4aa6e7bb8017e882330a7017e94be265949b5a
Chenyang Liu <zackliu1995@hotmail.com>
2017-07-04 14:55:02 +0800

    Add sample for connecting Windows Slave via SSH

---
+++ powershell/Jenkins-Windows-Init-Script-SSH.ps1
@@ -0,0 +1,44 @@
+# Download and Install Java
+Set-ExecutionPolicy Unrestricted
+#Default workspace location
+Set-Location C:\
+$source = "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-windows-x64.exe"
+$destination = "C:\jdk-8u131-windows-x64.exe"
+$client = new-object System.Net.WebClient
+$cookie = "oraclelicense=accept-securebackup-cookie"
+$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
+$client.downloadFile($source, $destination)
+$proc = Start-Process -FilePath $destination -ArgumentList "/s" -Wait -PassThru
+$proc.WaitForExit()
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", "c:\Program Files\Java\jdk1.8.0_131", "Machine")
+[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";c:\Program Files\Java\jdk1.8.0_131\bin", "Machine")
+$Env:Path += ";c:\Program Files\Java\jdk1.8.0_131\bin"
+
+
+# Install Maven
+$source = "http://mirror.reverse.net/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip"
+$destination = "C:\maven.zip"
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($source, $destination)
+$shell_app=new-object -com shell.application
+$zip_file = $shell_app.namespace($destination)
+mkdir 'C:\Program Files\apache-maven-3.5.0'
+$destination = $shell_app.namespace('C:\Program Files')
+$destination.Copyhere($zip_file.items(), 0x14)
+[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\Program Files\apache-maven-3.5.0\bin", "Machine")
+$Env:Path += ";C:\Program Files\apache-maven-3.5.0\bin"
+
+
+# Install Git
+$source = "https://github.com/git-for-windows/git/releases/latest"
+$latestRelease = Invoke-WebRequest -UseBasicParsing $source -Headers @{"Accept"="application/json"}
+$json = $latestRelease.Content | ConvertFrom-Json
+$latestVersion = $json.tag_name
+$versionHead = $latestVersion.Substring(1, $latestVersion.IndexOf("windows")-2)
+$source = "https://github.com/git-for-windows/git/releases/download/v${versionHead}.windows.1/Git-${versionHead}-64-bit.exe"
+$destination = "C:\Git-${versionHead}-64-bit.exe"
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($source, $destination)
+$proc = Start-Process -FilePath $destination -ArgumentList "/VERYSILENT" -Wait -PassThru
+$proc.WaitForExit()
+$Env:Path += ";C:\Program Files\Git\cmd"
