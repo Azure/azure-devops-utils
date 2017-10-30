9501a84c1c7e7f9fefaa29c4e960c1b833a1e2af
chenyl <chenyl@microsoft.com>
2017-10-30 17:53:17 +0800

    fix maven source

--- powershell/Jenkins-Windows-Init-Script-SSH.ps1
+++ powershell/Jenkins-Windows-Init-Script-SSH.ps1
@@ -18,17 +18,17 @@ git config --system --unset credential.helper
 
 
 # Install Maven
-$source = "http://mirror.reverse.net/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip"
+$source = "https://archive.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip"
 $destination = "C:\maven.zip"
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($source, $destination)
 $shell_app=new-object -com shell.application
 $zip_file = $shell_app.namespace($destination)
-mkdir 'C:\Program Files\apache-maven-3.5.0'
+mkdir 'C:\Program Files\apache-maven-3.5.2'
 $destination = $shell_app.namespace('C:\Program Files')
 $destination.Copyhere($zip_file.items(), 0x14)
-[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\Program Files\apache-maven-3.5.0\bin", "Machine")
-$Env:Path += ";C:\Program Files\apache-maven-3.5.0\bin"
+[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";C:\Program Files\apache-maven-3.5.2\bin", "Machine")
+$Env:Path += ";C:\Program Files\apache-maven-3.5.2\bin"
 
 
 # Install Git
