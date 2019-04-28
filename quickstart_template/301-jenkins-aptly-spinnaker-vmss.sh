be7520e46a98b35485bf5ff4cfadd4c3618985a1
sdpan <306465391@qq.com>
2019-04-28 12:41:01 +0800

    Update 301-jenkins-aptly-spinnaker-vmss.sh

--- quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
+++ quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
@@ -173,6 +173,7 @@ rm "addUser.groovy"
 port=8082
 sed -i -e "s/\(HTTP_PORT=\).*/\1$port/"  /etc/default/jenkins
 service jenkins restart
+hal deploy apply
 # Wait for Spinnaker services to be up before returning
 timeout=180
 echo "while !(nc -z localhost 8084) || !(nc -z localhost 9000); do sleep 1; done" | timeout $timeout bash
