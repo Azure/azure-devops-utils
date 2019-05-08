fbc78b1ad5a1854c5a46b36d224345b79580404d
sdpan <306465391@qq.com>
2019-05-08 16:15:51 +0800

    Update 301-jenkins-aptly-spinnaker-vmss.sh

--- quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
+++ quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
@@ -132,7 +132,7 @@ echo "$app_key" | hal config provider azure account add my-azure-account \
   --default-key-vault "$vault_name" \
   --default-resource-group "$resource_group" \
   --packer-resource-group "$resource_group" \
-  --regions "$region" \
+  --regions "eastus","westus","$region" \
   --app-key
 hal config provider azure enable
 
