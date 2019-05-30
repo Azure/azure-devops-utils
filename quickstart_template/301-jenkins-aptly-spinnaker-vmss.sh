d4242021bcd19be952bc658c922a97d5b7ffe6e0
sdpan <306465391@qq.com>
2019-05-30 14:27:15 +0800

    Update 301-jenkins-aptly-spinnaker-vmss.sh

--- quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
+++ quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
@@ -113,7 +113,12 @@ throw_if_empty region $region
 
 default_hal_config="/home/$jenkins_username/.hal/default"
 #install az cli and get-credentials from aks
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
+
 az login --service-principal -u "$app_id" -p "$app_key" -t "$tenant_id"
 az aks get-credentials --resource-group $resource_group --name $clusterName
 
