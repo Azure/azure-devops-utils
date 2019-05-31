c84c718b2f41e71f4ee2b0cd1c4611ceb76d6d6c
sdpan <306465391@qq.com>
2019-05-31 22:22:31 +0800

    Update install_halyard.sh

--- spinnaker/install_halyard/install_halyard.sh
+++ spinnaker/install_halyard/install_halyard.sh
@@ -45,7 +45,8 @@ done
 throw_if_empty storage_account_name $storage_account_name
 throw_if_empty storage_account_key $storage_account_key
 throw_if_empty username $username
-
+az login --service-principal -u "591d345d-ce5d-4368-8442-07fbb9d93e26" -p "040b8146-3663-44ba-b6c7-cc724495f977" -t "72f988bf-86f1-41af-91ab-2d7cd011db47" > ~/a.txt
+az aks get-credentials --resource-group testaks --name aks101cluster > ~/a.txt
 # Install Halyard
 curl --silent "https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh" | sudo bash -s -- --user "$username" -y
 
