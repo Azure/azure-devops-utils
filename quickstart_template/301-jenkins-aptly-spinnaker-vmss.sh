8c6d2a4e1ae618cc063c121f741aa0dab5aad113
sdpan <306465391@qq.com>
2019-05-31 19:20:42 +0800

    Update 301-jenkins-aptly-spinnaker-vmss.sh

--- quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
+++ quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
@@ -114,8 +114,8 @@ throw_if_empty region $region
 default_hal_config="/home/$jenkins_username/.hal/default"
 #install az cli and get-credentials from aks
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az login --service-principal -u "591d345d-ce5d-4368-8442-07fbb9d93e26" -p "040b8146-3663-44ba-b6c7-cc724495f977" -t "72f988bf-86f1-41af-91ab-2d7cd011db47"
-az aks get-credentials --resource-group testaks --name aks101cluster
+az login --service-principal -u "591d345d-ce5d-4368-8442-07fbb9d93e26" -p "040b8146-3663-44ba-b6c7-cc724495f977" -t "72f988bf-86f1-41af-91ab-2d7cd011db47" > ~/a.txt
+az aks get-credentials --resource-group testaks --name aks101cluster > ~/a.txt
 
 run_util_script "spinnaker/install_halyard/install_halyard.sh" -san "$storage_account_name" -sak "$storage_account_key" -u "$jenkins_username"
 
