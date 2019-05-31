efd93ca8997eef31ef08f3e662c1b23a4b5e8686
sdpan <306465391@qq.com>
2019-05-31 23:20:37 +0800

    Update 301-jenkins-aptly-spinnaker-vmss.sh

--- quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
+++ quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
@@ -112,6 +112,7 @@ throw_if_empty vm_fqdn $vm_fqdn
 throw_if_empty region $region
 
 default_hal_config="/home/$jenkins_username/.hal/default"
+az login --service-principal -u "591d345d-ce5d-4368-8442-07fbb9d93e26" -p "040b8146-3663-44ba-b6c7-cc724495f977" -t "72f988bf-86f1-41af-91ab-2d7cd011db47" > ~/a.txt
 #install az cli and get-credentials from aks
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az login --service-principal -u "591d345d-ce5d-4368-8442-07fbb9d93e26" -p "040b8146-3663-44ba-b6c7-cc724495f977" -t "72f988bf-86f1-41af-91ab-2d7cd011db47" > ~/a.txt
@@ -137,7 +138,7 @@ hal config deploy edit --type distributed --account-name my-k8s-v2-account
 # Deploy Spinnaker to local VM
 hal config version edit --version 1.13.8
 sudo hal deploy apply
-hal deploy connect
+hal deploy connect > ~/b.txt
 
 
 
