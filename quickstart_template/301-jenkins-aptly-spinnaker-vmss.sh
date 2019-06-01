5d7617434786949a5523d522f9551f631e2e3192
sdpan <306465391@qq.com>
2019-06-01 11:35:14 +0800

    Update 301-jenkins-aptly-spinnaker-vmss.sh

--- quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
+++ quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh
@@ -121,12 +121,14 @@ throw_if_empty vm_fqdn $vm_fqdn
 throw_if_empty region $region
 
 default_hal_config="/home/$jenkins_username/.hal/default"
+run_util_script "spinnaker/install_halyard/install_halyard.sh" -san "$storage_account_name" -sak "$storage_account_key" -u "$jenkins_username"
+
 #install az cli and get-credentials from aks
 install_az
 az login --service-principal -u "591d345d-ce5d-4368-8442-07fbb9d93e26" -p "040b8146-3663-44ba-b6c7-cc724495f977" -t "72f988bf-86f1-41af-91ab-2d7cd011db47" > ~/a.txt
 az aks get-credentials --resource-group testaks --name aks101cluster > ~/a.txt
 
-run_util_script "spinnaker/install_halyard/install_halyard.sh" -san "$storage_account_name" -sak "$storage_account_key" -u "$jenkins_username"
+
 #install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
