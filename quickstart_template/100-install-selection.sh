5b71973f0ae097f757c2b89f9aaee8f72e9df63d
onlyloveyouever <onlyloveyouever@126.com>
2019-06-11 16:53:00 +0800

    add some parameters

--- quickstart_template/100-install-selection.sh
+++ quickstart_template/100-install-selection.sh
@@ -94,11 +94,13 @@ throw_if_empty vault_name $vault_name
 throw_if_empty storage_account_name $storage_account_name
 throw_if_empty storage_account_key $storage_account_key
 throw_if_empty region $region
+throw_if_empty password $password
+throw_if_empty vm_fqdn $vm_fqdn
 
 #installed on the vmss if there is no aks_cluster_name, otherwise installed on aks
 if [ -z "$aks_cluster_name" ]
 then
-      run_util_script "quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh"  -ju "$username" -jp "$password" -ai "$app_id" -ak "$app_key" -ti "$tenant_id" -si "$subscription_id" -rg "$resource_group" -vn "$vault_name" -san "$storage_account_name" sak "$storage_account_key" -r "$region" -al "$artifacts_location"  -st "$artifacts_location_sas_token"
+      run_util_script "quickstart_template/301-jenkins-aptly-spinnaker-vmss.sh"  -ju "$username" -jp "$password" -ai "$app_id" -ak "$app_key" -ti "$tenant_id" -si "$subscription_id" -rg "$resource_group" -vn "$vault_name" -san "$storage_account_name" -sak "$storage_account_key" -vf "$vm_fqdn" -r "$region" -al "$artifacts_location" -st "$artifacts_location_sas_token"
 else
-      run_util_script "quickstart_template/101-spinnaker-aks.sh"  -u "$username" -ai "$app_id" -ak "$app_key" -ti "$tenant_id" -si "$subscription_id"  -rg "$resource_group"  -vn "$vault_name" -can "$aks_cluster_name" -san "$storage_account_name" sak "$storage_account_key" -r "$region" -al "$artifacts_location" -st "$artifacts_location_sas_token"
+      run_util_script "quickstart_template/101-spinnaker-aks.sh"  -u "$username" -ai "$app_id" -ak "$app_key" -ti "$tenant_id" -si "$subscription_id" -rg "$resource_group" -vn "$vault_name" -can "$aks_cluster_name" -san "$storage_account_name" -sak "$storage_account_key" -r "$region" -al "$artifacts_location" -st "$artifacts_location_sas_token"
 fi
